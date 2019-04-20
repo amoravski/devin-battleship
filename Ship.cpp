a4c1afe8f38fb82aa91ba5b71d1596f63c6cc86c
ilian02 <36775333+ilian02@users.noreply.github.com>
2019-04-20 10:13:11 +0300

    Add files via upload

--- Ship.cpp
+++ Ship.cpp
@@ -13,13 +13,13 @@ Ship::Ship()
 
 	while (1)
 	{
-		cout << "Rotation X or Y: ";
+		cout << "Rotation X or Y: ";				//Up-Down or Left-Right direction
 		cin >> rotation;
 		if (rotation == 'X' or rotation == 'Y')
 			break;
 	}
 
-	cout << "X and Y: " << endl;
+	cout << "X and Y: " << endl;					//Starting Coardinates
 	cin >> x;
 	cin >> y;
 
